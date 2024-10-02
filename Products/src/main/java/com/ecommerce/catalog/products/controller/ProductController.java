9e798f1e45b3b5fbdd6c1235c26fa0c36cb2d2ae
Ervin Triano <ervintdeveloper@gmail.com>
2024-10-02 13:50:27 -0600

    Fix: Fixed status code from OK to CREATED when product is created

--- Products/src/main/java/com/ecommerce/catalog/products/controller/ProductController.java
+++ Products/src/main/java/com/ecommerce/catalog/products/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -60,7 +61,7 @@ public class ProductController {
                 .product(savedProduct)
                 .message(message)
                 .build();
-        return ResponseEntity.ok(productResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
 
     }
 
