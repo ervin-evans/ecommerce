acb4c951b010bb2644ca38b1d490c5d97bc073e5
Ervin Triano <ervintdeveloper@gmail.com>
2024-10-09 09:14:58 -0600

    Feat: Add role model

---
+++ users-microservice/src/main/java/com/ecommerce/users/model/Role.java
@@ -0,0 +1,27 @@
+package com.ecommerce.users.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "BINARY(16)")
+    private UUID id;
+    
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+}
